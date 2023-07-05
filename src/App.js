394333a8aa349afe731c681a3c308ee4c886357c
firatToktas <frttkts0@gmail.com>
2023-07-05 19:44:28 +0300

    process.env

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 function App() {
   return (
     <div className="App">
+      <h3>{process.env.NODE_ENV}</h3>
       <img src='/logo192.png' alt=''/>
       <h1 style={{color:"red", backgroundColor:"tomato", display:"inline-block", height:"200px"}}>Test</h1>
     </div>
