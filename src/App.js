04bcb0c86a432ce88977e6c018eb61aa92a43138
firatToktas <frttkts0@gmail.com>
2023-07-08 12:26:32 +0300

    jsx ile elman oluşturma

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import styles from './App.module.css'
-import Test from './Test.js'
-import {Title}  from './Components.js';
-import Tailwind from './tailwind';
 import './tailwind.css';
+import { createElement } from 'react';
+
+function Button(props){
+  return <button>{props.text}</button>
+}
 
 function App() {
+  // const h1=createElement('h1', null, 'eben be eben');
+  // return createElement('main',{
+    //   className:'anan',
+    //   id:'eben'
+    // },h1)
+  const todos=['todo1', 'todo2', 'todo3'];
+  const name="Anan be anan";
+  let num=0;
+  function anan(){
+    num++
+    console.log('lim'+ num);
+  }
   return (
-    <div className={styles.App}>
-      <p>
-        {process.env.REACT_APP_API_URL}
-      </p>
-
-      <Title>{process.env.NODE_ENV}</Title>
-      <img src='/logo192.png' alt=''/>
-      <h1>Test</h1>
-      <Test/>
-      <Tailwind/>
-    </div>
+    <main>
+      <h1 style={{color:'tomato', fontWeight:'700', fontSize:'45px'}}>Anan be anan</h1>
+      <label htmlFor='search' onClick={anan}>Arama</label>
+      <input type='text' id='search' tabIndex="1"/>
+      <Button text='merhaba aq'/>
+      <ul>
+        {todos.map((todo, index)=>
+            <li key={index}>
+              {todo +" "+ name.toUpperCase()}
+            </li>
+        )}
+      </ul>
+    </main>
   );
 }
 
---
+++ src/style.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import styles from './App.module.css'
+import Test from './Test.js'
+import {Title}  from './Components.js';
+import Tailwind from './tailwind';
+
+function Styles(){
+    return(
+        <div className={styles.App}>
+        <p>
+          {process.env.REACT_APP_API_URL}
+        </p>
+  
+        <Title>{process.env.NODE_ENV}</Title>
+        <img src='/logo192.png' alt=''/>
+        <h1>Test</h1>
+        <Test/>
+        <Tailwind/>
+      </div>
+    )
+}
+
+export default Styles
