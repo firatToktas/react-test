fbe9db029611a2b211b9de86505e6b2070974829
firatToktas <frttkts0@gmail.com>
2023-07-17 20:38:57 +0300

    search filtreleme eklendi

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,10 +2,11 @@ import { useState } from "react";
 import Header from "./Memoization";
 function TodoApp(){
 
-    console.log('app render');
     const [todo, setTodo]=useState('');
     const [todos, setTodos]=useState([]);
-
+    
+    const [search, setSearch]=useState('');
+    
     const addTodo=(e)=>{
         e.preventDefault();
         setTodo(e.target.value);
@@ -13,20 +14,35 @@ function TodoApp(){
     const printTodo=()=>{
         setTodos([...todos, todo]);
         setTodo('');
-        console.log('gönderdi');
     }
+    const searchHandle= e =>{
+        setSearch(e.target.value);
+    }
+
+    const filtredTodos = todos.filter((filtTodo) =>
+    filtTodo.toLowerCase().includes(search.toLowerCase()));
+
+    console.log(filtredTodos);
+    
     return (
         <>  
         <Header></Header>
+        <p style={{display:"inline", margin:"8px"}}>Arama yap</p>
+        <input type="text" value={search} onChange={searchHandle}/>
+        {search}
+        <hr></hr>
         <form onSubmit={addTodo}>
             <input type="text" value={todo} onChange={addTodo}/>
             <button type="submit" onClick={printTodo} disabled={!todo}>Ekle</button>
         </form>
-            <ul>
-            {todos.map((todo, index)=>(
-                <li key={index}>{todo}</li>
-                ))}
-            </ul>
+        <ul>
+            {todos.map((todo, index) => {
+                if (!search || filtredTodos.includes(todo)){
+                return <li key={index}>{todo}</li>;
+                }
+                return null;
+            })}
+        </ul>
         </>
     )
 }
